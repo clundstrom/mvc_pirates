690396394a440c053289be3e9270296d1c0b462d
Christoffer Lundström <lulleh_@hotmail.com>
2019-09-26 20:43:12 +0200

    added gradle to project

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import controller.MemberController;
 import view.Menu;
 
@@ -5,11 +6,10 @@ public class Program {
 
 
     public static void main(String[] args) {
-
         MemberController mc = new MemberController();
         Menu menu = new Menu();
         menu.addSubscriber(mc);
-
+        Gson json = new Gson();
         menu.presentation();
 
     }
