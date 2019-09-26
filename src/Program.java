b61d8347f33f7a59225391c9948b33ee3b6333b1
Christoffer Lundström <lulleh_@hotmail.com>
2019-09-26 12:00:22 +0200

    added view class

--- src/Program.java
+++ src/Program.java
@@ -8,16 +8,7 @@ public class Program {
 
     public static void main(String[] args) {
 
-        ArrayList<Boat> boatList = new ArrayList<>();
 
-        Boat boat = new Kayak("Greta", "BigPlay", 20);
-        Boat boat1 = new Canoe("Babe", "Big", 10);
-
-        boatList.add(boat);
-        boatList.add(boat1);
-
-        String uniqueID = UUID.randomUUID().toString();
-        System.out.println(uniqueID);
 
     }
 
