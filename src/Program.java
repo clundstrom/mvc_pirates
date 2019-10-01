a1c14e19a57a52abf552a16385fda9c353fa4672
Marcus Johansson <mj223vn@student.lnu.se>
2019-10-01 20:25:12 +0200

    merge fix

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,13 @@
 import controller.RegisterController;
 import view.MenuView;
-
+import controller.BaseController;
 import java.io.File;
 
 public class Program {
 
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
         BaseController bs = new BaseController(new File("src\\assets\\database.json"), null);
->>>>>>> 231e6201b016d9d4ffcbbb1843314bc88892cb07
         RegisterController registerController = new RegisterController();
 
         MenuView menu = new MenuView(registerController);
